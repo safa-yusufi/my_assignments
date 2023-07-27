79b1a32f2d899f223acf70e1d08f4e724dca1015
safa <safayosufi453@gmail.com>
2023-07-27 18:15:16 +0500

    my assignments

---
+++ assignment#11/setmetod_differenceupdate.py
@@ -0,0 +1,4 @@
+x = {"gulsom", "sanya", "arya", "marya"}
+y = {"sara", "sahar", "marya"}
+z = x.difference_update(y)
+print(z)
--- setmetod_differenceupdate.py
+++
@@ -1,4 +0,0 @@
-x = {"gulsom", "sanya", "arya", "marya"}
-y = {"sara", "sahar", "marya"}
-z = x.difference_update(y)
-print(z)
